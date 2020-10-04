6e6b7bf05daef49c6a3508a367c47b89aaaece46
Lilly7777 <37080953+Lilly7777@users.noreply.github.com>
2020-10-04 10:31:48 +0300

    Update make_offer.php

--- make_offer.php
+++ make_offer.php
@@ -4,8 +4,6 @@ error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] =='POST'){
 
- //   $categoryID = $_POST['categoryID'];
-    //$firmName = $_POST('firmName');
     $date_from_to=$_POST['date_from_to'];
     $coupon_countstr=$_POST['coupon_count'];
     $coupon_count=(int)$coupon_countstr;
@@ -13,12 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
     $business_user_idstr=$_POST['business_user_id'];
     $business_user_id=(int)$business_user_idstr;
     $description = $_POST['description'];
-    //$address = $_POST('address');
     $pointsstr = $_POST['points'];
     $points=(int)$pointsstr;
-    //$unitsPerOffer = $_POST('unitsPerOffer');
-    //$bussinessHours = $_POST('bussinessHours');
-    //$dateFromToDateDue = $_POST('dateFromToDateDue');
     require_once 'connect.php';
 
     $sql = "INSERT INTO offers (date_from_to, coupon_count, business_user_id, description, points) VALUES ('$date_from_to', '$coupon_count', '$business_user_id','$description', '$points')";
