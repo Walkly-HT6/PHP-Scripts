7dea22c7a54ec5733aba00e9849d33f821af1f25
Lilly7777 <37080953+Lilly7777@users.noreply.github.com>
2020-10-04 13:08:26 +0300

    Create detail_user.php

---
+++ detail_user.php
@@ -0,0 +1,55 @@
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] =='POST'){
+
+	$email = $_POST['email'];
+
+	require_once 'connect.php';
+
+	$sql = "SELECT * FROM users WHERE email='$email' ";
+
+	$response = mysqli_query($conn, $sql);
+
+	$num_rows = mysqli_num_rows($response);
+
+	if($num_rows === 1){
+		$row = mysqli_fetch_assoc($response);
+
+		$user_details['first_name'] = $row['first_name'];
+		$user_details['last_name'] = $row['last_name'];
+		$user_details['email'] = $row['email'];
+		$user_details['joined_date'] = $row['joined_date'];
+
+
+		echo json_encode($user_details);
+		mysqli_close($conn);
+
+
+	} else{
+
+		$sql = "SELECT * FROM business_users WHERE email='$email' ";
+
+		$response = mysqli_query($conn, $sql);	
+
+		if($num_rows === 1){
+			$row = mysqli_fetch_assoc($response);
+			$user_details['company_name'] = $row['company_name'];
+			$user_details['business_hours'] = $row[' business_hours'];
+			$user_details['offers'] = $row['offers'];
+			$user_details['first_name'] = $row['first_name'];
+			$user_details['last_name'] = $row['last_name'];
+			$user_details['phone_number'] = $row['phone_number'];
+			$user_details['description'] = $row['description'];
+			$user_details['email'] = $row['email'];
+
+			
+			echo json_encode($user_details);
+			mysqli_close($conn);
+
+
+		}
+	}
+	
+}
+	
+?>
