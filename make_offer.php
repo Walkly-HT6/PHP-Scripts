b1560e233229dd48e3028ce2f1335438c658c3ce
Angel Stoyanov <48317633+AngelStoyanov33@users.noreply.github.com>
2020-10-04 16:33:16 +0300

    Update make_offer.php

--- make_offer.php
+++ make_offer.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -7,25 +8,19 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
     $date_from_to=$_POST['date_from_to'];
     $coupon_countstr=$_POST['coupon_count'];
     $coupon_count=(int)$coupon_countstr;
-
     $business_user_idstr=$_POST['business_user_id'];
     $business_user_id=(int)$business_user_idstr;
     $description = $_POST['description'];
     $pointsstr = $_POST['points'];
     $points=(int)$pointsstr;
+
     require_once 'connect.php';
-
-    $sql = "INSERT INTO offers (date_from_to, coupon_count, business_user_id, description, points) VALUES ('$date_from_to', '$coupon_count', '$business_user_id','$description', '$points')";
-    if ( mysqli_query($conn, $sql) ) {
+	
+    $sql = "INSERT INTO offers (date_from_to, coupon_count, business_user_id, description, points) VALUES ('$date_from_to', '$coupon_count', '$business_user_id','$description','$points');";
+    if (mysqli_query($conn, $sql) ) {
         $result = "1";
+		mysqli_close($conn);
 
-        mysqli_close($conn);
-
-    } else {
-
-        $result = "0";
-        mysqli_close($conn);
-    }
-}
-
+		}
+	}	
 ?>
