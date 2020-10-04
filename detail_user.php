0a69ce23da58084931371fc7a0f469c74893fe28
Angel Stoyanov <48317633+AngelStoyanov33@users.noreply.github.com>
2020-10-04 16:54:02 +0300

    Update detail_user.php

--- detail_user.php
+++ detail_user.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
                 $user_details['last_name'] = $row['last_name'];
                 $user_details['email'] = $row['email'];
                 $user_details['joined_date'] = $row['joined_date'];
+				$user_details['is_business'] = "false";
 
 
                 echo json_encode($user_details);
@@ -37,13 +38,14 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
                 if($num_rows === 1){
                         $row = mysqli_fetch_assoc($response);
                         $user_details['company_name'] = $row['company_name'];
-                                                $user_details['business_hours'] = $row[' business_hours'];
+						$user_details['business_hours'] = $row[' business_hours'];
                         $user_details['offers'] = $row['offers'];
                         $user_details['first_name'] = $row['first_name'];
                         $user_details['last_name'] = $row['last_name'];
                         $user_details['phone_number'] = $row['phone_number'];
-						$user_details['description'] = $row['description'];
+                        $user_details['description'] = $row['description'];
                         $user_details['email'] = $row['email'];
+						$user_details['is_business'] = "true";
 
 
                         echo json_encode($user_details);
@@ -56,7 +58,3 @@ if ($_SERVER['REQUEST_METHOD'] =='POST'){
 
 }
 ?>
-
-						
-						
-						
